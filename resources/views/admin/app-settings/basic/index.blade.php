197a44b643436f2821196e2ea8d9750096e7e2cd
Shahrukh A. Khan <iam@shaz3e.com>
2023-12-15 20:59:20 +0500

    route fixed app-setting

--- resources/views/admin/app-settings/basic/index.blade.php
+++ resources/views/admin/app-settings/basic/index.blade.php
@@ -19,7 +19,7 @@
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{ DiligentCreators('dashboard_url') }}">Home</a></li>
-                            <li class="breadcrumb-item"><a href="javascript:void(0);">App Settings</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.app-settings') }}">App Settings</a></li>
                             <li class="breadcrumb-item active">App Basic Settings</li>
                         </ol>
                     </div>
